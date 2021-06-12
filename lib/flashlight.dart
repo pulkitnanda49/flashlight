30510763e340cadb3c65d0e6ce053ba29454df29
Pulkit <pulkit@baliza.io>
2021-06-12 13:33:30 +0530

    refractored to 2.0

--- lib/flashlight.dart
+++ lib/flashlight.dart
@@ -3,20 +3,20 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class Flashlight {
-  static const MethodChannel _channel = const MethodChannel('flashlight');
+  static const MethodChannel? _channel = const MethodChannel('flashlight');
 
   static Future<bool> get hasFlashlight async {
-    final bool _hasFlashlight = await _channel.invokeMethod('hasFlashlight');
-    return _hasFlashlight;
+    final bool? _hasFlashlight = await _channel?.invokeMethod('hasFlashlight');
+    return (_hasFlashlight ?? false);
   }
 
   static Future<void> lightOn() async {
-    await _channel.invokeMethod('lightOn');
+    await _channel?.invokeMethod('lightOn');
     return;
   }
 
   static Future<void> lightOff() async {
-    await _channel.invokeMethod('lightOff');
+    await _channel?.invokeMethod('lightOff');
     return;
   }
 }
